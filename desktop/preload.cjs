bddff2f538348c578546c57a16b9200825914c09
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-06 19:41:38 +0200

    feat: expose desktop diagnostics folder action

--- desktop/preload.cjs
+++ desktop/preload.cjs
@@ -2,6 +2,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("studyosDesktop", {
   getStartupError: () => ipcRenderer.invoke("studyos:get-startup-error"),
+  openDiagnostics: () => ipcRenderer.invoke("studyos:open-diagnostics"),
   useLocalBackend: () => ipcRenderer.invoke("studyos:use-local-backend"),
   saveBackendUrl: (backendUrl) =>
     ipcRenderer.invoke("studyos:save-backend", backendUrl),
